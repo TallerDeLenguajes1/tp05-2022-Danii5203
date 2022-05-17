3d9ddf4686053acf3eb9ab268d4a2973a9595d7f
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-17 16:22:22 -0300

    commit 5

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,3 +6,11 @@ Console.WriteLine("2do hola mundo");
 Console.WriteLine("3er hola mundo");
 
 Console.WriteLine("add, commit, push desde la consola de vscode");
+string texto;
+texto = Console.ReadLine();
+string n1 = Console.ReadLine();
+string n2 = Console.ReadLine();
+
+double res = Convert.ToDouble(n1) + Convert.ToDouble(n2);
+
+Console.WriteLine("resultado: " + res);
