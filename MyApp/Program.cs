b9eaab564798036adadd93213993349a4dfcd154
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-17 17:01:34 -0300

    2do commit branch primer branch

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,6 +8,7 @@ Console.WriteLine("3er hola mundo");
 Console.WriteLine("add, commit, push desde la consola de vscode");
 string texto;
 texto = Console.ReadLine();
+
 string n1 = Console.ReadLine();
 string n2 = Console.ReadLine();
 
