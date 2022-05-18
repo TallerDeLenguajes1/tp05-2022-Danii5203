a5cfd3c5d917604250a6eb99a7befe4f315925cd
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-18 12:34:07 -0300

    ejercicio1 terminado(invertir un numero mayor a 0)

---
+++ Ejercicio1/ejercicio1.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+Console.WriteLine("Ingrese un numero: ");
+string numero = Console.ReadLine();
+
+if(Convert.ToInt32(numero)>0){
+    string numInvertido = invertirNumero(numero);
+    Console.WriteLine("El numero invertido es: " + numInvertido);
+}else{
+    Console.WriteLine("El numero menor o igual a 0: "+numero);
+}
+
+string invertirNumero(string n){
+    string numInverso = String.Empty; //inicializamos el string vacio
+    for (int i=n.Length-1; i>=0; i--) //recorremos el numero de atras hacia adelante
+    {
+        numInverso += n[i];
+    }
+    return numInverso;
+}
