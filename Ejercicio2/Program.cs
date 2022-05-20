0a349bca410658b85f15fe084f3da56d097d2fa3
lucas5203 <lucasdanielbrandan0@gmail.com>
2022-05-20 14:21:20 -0300

    2do commit Calculadora_V2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 Console.WriteLine("Hello, World!");
 
-using System;
 
 //version 2
 Console.WriteLine("Ingrese un valor: ");
@@ -14,3 +14,20 @@ Console.WriteLine("La raiz cuadrada de "+n3+" = "+Math.Sqrt(n3));
 Console.WriteLine("El Seno de "+n3+" = "+Math.Sin(n3));
 Console.WriteLine("El Coseno de "+n3+" = "+Math.Cos(n3));
 Console.WriteLine("Redondeo el numero flotante "+n3+" = "+Math.Round(n3));
+
+Console.WriteLine("Ingrese un primer valor: ");
+double valor1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese un segundo valor: ");
+double valor2 = Convert.ToDouble(Console.ReadLine());
+
+if(valor1 > valor2){
+    Console.WriteLine("El valor maximo (valor 1): "+ valor1);
+    Console.WriteLine("El valor minimo (valor 2): "+ valor2);
+}else{
+    if(valor2 > valor1){
+        Console.WriteLine("El valor maximo (valor 2): "+ valor2);
+        Console.WriteLine("El valor minimo (valor 1): "+ valor1);
+    }else{
+        Console.WriteLine(valor1+"(valor 1) es igual a "+valor2+"(valor 2).");
+    }
+}
